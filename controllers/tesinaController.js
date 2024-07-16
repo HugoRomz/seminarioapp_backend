1f891b83323fcef59330bf85d7bf573aedf8acb1
Carlos <charlessalberto15@gmail.com>
2024-07-16 17:45:02 -0600

    Se agrego la vista de registro de tesina v3

--- controllers/tesinaController.js
+++ controllers/tesinaController.js
@@ -1,36 +1,193 @@
-import { Carreras } from "../models/Carreras.js";
-import { Cursos } from "../models/Cursos.js";
-import { CursoPeriodos, Periodos } from "../models/Periodo.js";
 import { Usuarios } from "../models/Usuarios.js";
-import { Invitaciones } from "../models/Tesinas.js";
-import {
-  handleBadRequestError,
-  handleInternalServerError,
-} from "../Utils/index.js";
+import { Invitaciones, Tesinas } from "../models/Tesinas.js";
+import { sendEmailInvitation } from "../emails/authEmailService.js";
 
 const createInvitation = async (req, res) => {
     try {
         const { nombre_tesina, area_tema, resenia_tema, userId, invitado_email } = req.body;
-        console.log(req.body)
-        const invitado = await Usuarios.findOne({ where: { email_usuario: invitado_email } });
-        console.log(invitado.usuario_id)
-        if (!invitado) {
-            return res.status(404).json({ error: 'Compañero no encontrado' });
+
+        if (!Array.isArray(invitado_email) || invitado_email.length === 0) {
+            return res.status(400).json({ error: 'Debe proporcionar al menos un correo electrónico.' });
         }
 
-        const nuevaInvitacion = await Invitaciones.create({
-            nombre_tesina,
-            area_tema,
-            resenia_tema,
-            usuario_id: userId,
-            usuario_id_invitado: invitado.usuario_id
-        });
+        const failedInvitations = [];
+        const successfulInvitations = [];
+        const pendingInvitations = [];
 
-        res.status(201).json(nuevaInvitacion);
+        for (const email of invitado_email) {
+            const invitado = await Usuarios.findOne({ where: { email_usuario: email } });
+
+            if (!invitado) {
+                failedInvitations.push(email);
+                continue;
+            }
+
+            const invitadoSentInvitation = await Invitaciones.findOne({ where: { usuario_id: invitado.usuario_id } });
+            if (invitadoSentInvitation) {
+                pendingInvitations.push(email);
+                continue;
+            }
+
+            const pendingInvitation = await Invitaciones.findOne({ where: { usuario_id_invitado: invitado.usuario_id } });
+            if (pendingInvitation) {
+                pendingInvitations.push(email);
+                continue;
+            }
+
+            const nuevaInvitacion = await Invitaciones.create({
+                nombre_tesina,
+                area_tema,
+                resenia_tema,
+                usuario_id: userId,
+                usuario_id_invitado: invitado.usuario_id
+            });
+
+            successfulInvitations.push(nuevaInvitacion);
+
+            // Envía el correo de invitación al invitado
+            await sendEmailInvitation(email, req.user.nombre, nombre_tesina, area_tema, resenia_tema);
+        }
+
+        const responseMessage = {
+            message: 'Proceso de invitaciones completado.',
+            successfulInvitations,
+            failedInvitations,
+            pendingInvitations
+        };
+
+        res.status(201).json(responseMessage);
     } catch (error) {
-        console.error('Error al crear la invitación:', error);
-        res.status(500).json({ error: 'Error al crear la invitación' });
+        console.error('Error al crear las invitaciones:', error);
+        res.status(500).json({ error: 'Error al crear las invitaciones' });
     }
 };
 
-export { createInvitation };
+const getUserInvitations = async (req, res) => {
+    try {
+        const { userId } = req.params;
+
+        const invitaciones = await Invitaciones.findAll({
+            where: { usuario_id: userId },
+            include: [
+                {
+                    model: Usuarios,
+                    attributes: ['nombre', 'apellido_p', 'apellido_m', 'email_usuario'],
+                    required: true
+                }
+            ]
+        });
+        res.status(200).json(invitaciones);
+    } catch (error) {
+        console.error('Error al obtener las invitaciones:', error);
+        res.status(500).json({ error: 'Error al obtener las invitaciones' });
+    }
+};
+
+const getInvitationsForUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const invitations = await Invitaciones.findAll({
+            where: { usuario_id_invitado: userId },
+            include: [
+                {
+                    model: Usuarios,
+                    attributes: ['nombre', 'apellido_p', 'apellido_m', 'email_usuario'],
+                    required: true
+                }
+            ]
+        });
+        res.status(200).json(invitations);
+    } catch (error) {
+        console.error('Error al obtener las invitaciones:', error);
+        res.status(500).json({ error: 'Error al obtener las invitaciones' });
+    }
+};
+
+const acceptInvitation = async (req, res) => {
+    try {
+        const { invitacionId } = req.params;
+
+        const invitacion = await Invitaciones.findByPk(invitacionId);
+
+        if (!invitacion) {
+            return res.status(404).json({ error: 'Invitación no encontrada' });
+        }
+
+        invitacion.status = 'ACEPTADO';
+        await invitacion.save();
+
+        res.status(200).json({ message: 'Invitación aceptada' });
+    } catch (error) {
+        console.error('Error al aceptar la invitación:', error);
+        res.status(500).json({ error: 'Error al aceptar la invitación' });
+    }
+};
+
+const rejectInvitation = async (req, res) => {
+    try {
+        const { invitacionId } = req.params;
+
+        const invitacion = await Invitaciones.findByPk(invitacionId);
+
+        if (!invitacion) {
+            return res.status(404).json({ error: 'Invitación no encontrada' });
+        }
+
+        await invitacion.destroy();
+
+        res.status(200).json({ message: 'Invitación rechazada' });
+    } catch (error) {
+        console.error('Error al rechazar la invitación:', error);
+        res.status(500).json({ error: 'Error al rechazar la invitación' });
+    }
+};
+
+const createTesina = async (req, res) => {
+    try {
+        const { nombre_tesina, area_tema, resenia_tema, userId } = req.body;
+
+        const nuevaTesina = await Tesinas.create({
+            usuario_id_docente: null,
+            usuario_id_alumno: userId,
+            nombre_tesina: nombre_tesina,
+            area_tesina: area_tema,
+            resenia_tesina: resenia_tema,
+            fecha_registro: new Date(),
+            status: 'PENDIENTE',
+            url_documento: null
+        });
+
+        res.status(201).json({ message: 'Tesina registrada con éxito.', tesina: nuevaTesina });
+    } catch (error) {
+        console.error('Error al registrar la tesina:', error);
+        res.status(500).json({ error: 'Error al registrar la tesina' });
+    }
+};
+
+const getTesinasByUser = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const tesinas = await Tesinas.findAll({
+            where: { usuario_id_alumno: userId }
+        });
+
+        if (tesinas.length === 0) {
+            return res.status(404).json({ message: 'No se encontraron tesinas para este usuario.' });
+        }
+
+        res.status(200).json(tesinas);
+    } catch (error) {
+        console.error('Error al obtener las tesinas del usuario:', error);
+        res.status(500).json({ message: 'Error al obtener las tesinas del usuario.', error });
+    }
+};
+
+export {
+    createInvitation,
+    getUserInvitations,
+    getInvitationsForUser,
+    acceptInvitation,
+    rejectInvitation,
+    createTesina,
+    getTesinasByUser
+};
--- routes/tesinaRoutes.js
+++ routes/tesinaRoutes.js
@@ -3,16 +3,64 @@ import authMiddleware from "../middleware/authMiddleware.js";
 import verificarRol from "../middleware/verificarRol.js";
 
 import {
-    createInvitation
+    createInvitation,
+    getUserInvitations,
+    getInvitationsForUser,
+    acceptInvitation,
+    rejectInvitation,
+    createTesina,
+    getTesinasByUser
 } from "../controllers/tesinaController.js";
 
 const router = Router();
+
+router.get(
+    "/invitaciones/:userId",
+    authMiddleware,
+    verificarRol(["Alumno"]),
+    getUserInvitations
+);
+
+router.get(
+    "/invitaciones/invitado/:userId",
+    authMiddleware,
+    verificarRol(["Alumno"]),
+    getInvitationsForUser
+);
 
 router.post(
     "/invitaciones",
     authMiddleware,
     verificarRol(["Alumno"]),
     createInvitation
+);
+
+router.post(
+    "/tesinas",
+    authMiddleware,
+    verificarRol(["Alumno"]),
+    createTesina
+);
+
+router.put(
+    "/invitaciones/aceptar/:invitacionId",
+    authMiddleware,
+    verificarRol(["Alumno"]),
+    acceptInvitation
+);
+
+router.put(
+    "/invitaciones/rechazar/:invitacionId",
+    authMiddleware,
+    verificarRol(["Alumno"]),
+    rejectInvitation
+);
+
+router.get(
+    "/tesinas/:userId",
+    authMiddleware,
+    verificarRol(["Alumno"]),
+    getTesinasByUser
 );
 
 export default router;
